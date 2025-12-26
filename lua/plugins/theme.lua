4673a03fc0e1bcf7735d990974ae807771561ac0
fmf <fmf@.com>
2025-12-26 23:24:42 +0800

    feat:还是切换为gruvbox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,55 +1,22 @@
 return {
-    -- 添加 Catppuccin 主题
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = false,
+        "ellisonleao/gruvbox.nvim",
         priority = 1000,
         config = function()
-            require("catppuccin").setup({
-                flavour = "mocha", -- 可选: mocha, latte, frappe, macchiato
-                no_italic = true, -- 禁用斜体
-                no_bold = false, -- 保留粗体
-                transparent_background = false,
-                show_end_of_buffer = false,
-                styles = {
-                    comments = {}, -- 禁用注释斜体
-                    keywords = {}, -- 禁用关键字斜体
-                    functions = {}, -- 禁用函数斜体
-                    variables = {}, -- 禁用变量斜体
-                    conditionals = {},
-                    loops = {},
-                    booleans = {},
-                    numbers = {},
-                    properties = {},
-                    types = {},
-                    operators = {},
-                },
-                integrations = {
-                    telescope = true,
-                    which_key = true,
-                    mason = true,
-                    neotree = true,
-                    aerial = true,
-                    notify = true,
-                    nvimtree = true,
-                    cmp = true,
-                    gitsigns = true,
-                    illuminate = true,
-                    indent_blankline = {
-                        enabled = true,
-                        scope_color = "surface2",
-                    },
+            require("gruvbox").setup({
+                terminal_colors = true,
+                underline = true,
+                contrast = "hard",
+                bold = false,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
                 },
             })
-            vim.cmd("colorscheme catppuccin")
+            vim.cmd.colorscheme("gruvbox")
         end,
-    },
-    -- 配置 LazyVim
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        },
     },
 }
