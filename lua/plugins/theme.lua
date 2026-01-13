63aff518140553aeb4efcea085468090138f879e
fmf <fmf@.com>
2026-01-13 23:01:33 +0800

    feat:删除光标光滑的插件

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,39 +1,33 @@
 return {
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = false,
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
         config = function()
-            require("catppuccin").setup({
-                flavour = "mocha", -- 可选: mocha, latte, frappe, macchiato
-                no_italic = true, -- 禁用斜体
-                no_bold = false, -- 保留粗体
-                transparent_background = false,
-                show_end_of_buffer = false,
-                integrations = {
-                    telescope = true,
-                    which_key = true,
-                    mason = true,
-                    neotree = true,
-                    aerial = true,
-                    notify = true,
-                    nvimtree = true,
-                    cmp = true,
-                    gitsigns = true,
-                    illuminate = true,
-                    indent_blankline = {
-                        enabled = true,
-                        scope_color = "surface2",
-                    },
+            require("gruvbox").setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = true,
+                    comments = false,
+                    operators = false,
+                    folds = true,
                 },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true,
+                contrast = "hard",
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
             })
-            vim.cmd("colorscheme catppuccin")
+            vim.cmd.colorscheme("gruvbox")
         end,
-    },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        },
     },
 }
