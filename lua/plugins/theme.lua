f5e85562f8c655c15376fd58eb4d0d6f5b607212
fmf <929433643@qq.com>
2026-01-29 08:45:11 +0800

    feat:添加truecolor色彩支持

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -37,57 +37,57 @@ return {
     --     },
     --
     -- },
-    -- {
-    --     "ellisonleao/gruvbox.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         require("gruvbox").setup({
-    --             terminal_colors = true,
-    --             undercurl = true,
-    --             underline = true,
-    --             bold = false,
-    --             italic = {
-    --                 strings = false,
-    --                 emphasis = true,
-    --                 comments = true,
-    --                 operators = false,
-    --                 folds = true,
-    --             },
-    --             strikethrough = true,
-    --             invert_selection = false,
-    --             invert_signs = false,
-    --             invert_tabline = false,
-    --             invert_intend_guides = false,
-    --             inverse = true,
-    --             contrast = "",
-    --             palette_overrides = {},
-    --             transparent_mode = false,
-    --         })
-    --         vim.cmd.colorscheme("gruvbox")
-    --     end,
-    -- },
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
         config = function()
-            require("rose-pine").setup({
-                variant = "moon", -- auto, main, moon, or dawn
-                dark_variant = "main", -- main, moon, or dawn
-                dim_inactive_windows = false,
-                extend_background_behind_borders = true,
-                enable = {
-                    terminal = true,
-                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-                    migrations = true, -- Handle deprecated options automatically
+            require("gruvbox").setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = true,
+                bold = false,
+                italic = {
+                    strings = false,
+                    emphasis = true,
+                    comments = false,
+                    operators = false,
+                    folds = true,
                 },
-                styles = {
-                    bold = true,
-                    italic = false,
-                    transparency = false,
-                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true,
+                contrast = "",
+                palette_overrides = {},
+                transparent_mode = false,
             })
-
-            vim.cmd("colorscheme rose-pine")
+            vim.cmd.colorscheme("gruvbox")
         end,
     },
+    --  {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     config = function()
+    --         require("rose-pine").setup({
+    --             variant = "moon", -- auto, main, moon, or dawn
+    --             dark_variant = "moon", -- main, moon, or dawn
+    --             dim_inactive_windows = false,
+    --             extend_background_behind_borders = true,
+    --             enable = {
+    --                 terminal = true,
+    --                 legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    --                 migrations = true, -- Handle deprecated options automatically
+    --             },
+    --             styles = {
+    --                 bold = true,
+    --                 italic = false,
+    --                 transparency = false,
+    --             },
+    --         })
+    --
+    --         vim.cmd("colorscheme rose-pine")
+    --     end,
+    -- },
 }
