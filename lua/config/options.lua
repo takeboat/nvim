cf215ba12521c2141324d475e1fd2ad930586890
fmf <929433643@qq.com>
2026-01-30 14:06:02 +0800

    feat:修改curosr style为block

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.tabstop = 4 -- tab 键显示为 4 个空格
-vim.opt.softtabstop = 4 -- 编辑模式下 tab 键插入 4 个空格
 vim.opt.shiftwidth = 4 -- 缩进使用 4 个空格
+vim.opt.softtabstop = 4 -- 编辑模式下 tab 键插入 4 个空格
 vim.opt.expandtab = true -- 将 tab 转换为空格
 vim.opt.smartindent = true -- 智能缩进
 vim.opt.termguicolors = true --启用gui色彩
 vim.opt.cursorline = false
+vim.opt.guicursor = "i:block"
